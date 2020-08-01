58ef4d2048e9c6aa4085f94116ba18aec30c0aa2
Shanuka Prabodha <shanukaprabodha1262@gmail.com>
2020-08-02 01:20:10 +0530

    initialize tutorial2

---
+++ app/src/main/java/com/example/usingstrings/ActivityOne.java
@@ -0,0 +1,52 @@
+package com.example.usingstrings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class ActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+
+       TextView tvMsg2 = findViewById(R.id.tvMsg2);
+
+       tvMsg2.setText(R.string.Msg2);
+
+        Log.i("Lifecycle","OnCreate() invoked");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Lifecycle","OnStart() invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Lifecycle","OnResume() invoked");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Lifecycle","OnPause() invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Lifecycle","OnStop() invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Lifecycle","OnDestroy() invoked");
+    }
+}
